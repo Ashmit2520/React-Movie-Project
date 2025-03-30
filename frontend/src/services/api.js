2e7e92c12ef10e52ceceaa18ea406a4be1b2289f
Ashmit2520 <105515137+Ashmit2520@users.noreply.github.com>
2025-03-29 20:18:45 -0500

    Initial commit

---
+++ frontend/src/services/api.js
@@ -0,0 +1,19 @@
+const API_KEY = "2577ceb20a6fed27b9b02e7c20c0341b";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
